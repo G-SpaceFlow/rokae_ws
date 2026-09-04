ac361b27e3fc738d8b94685d4a58e3702fa6ceff
Hamster-Li <1330983040@qq.com>
2026-09-04 16:12:43 +0800

    Update ROS 2 API documentation

--- assets/ROS2_INTERFACE_REFERENCE.md.kb4XmI_p.lean.js
+++ assets/ROS2_INTERFACE_REFERENCE.md.kb4XmI_p.lean.js
@@ -1,9 +1,9 @@
 import { _ as _export_sfc, o as openBlock, c as createElementBlock, a2 as createStaticVNode } from "./chunks/framework.BbW302T5.js";
-const __pageData = JSON.parse('{"title":"Rokae 双臂 ROS 2 接口技术文档","description":"","frontmatter":{},"headers":[],"relativePath":"ROS2_INTERFACE_REFERENCE.md","filePath":"ROS2_INTERFACE_REFERENCE.md","lastUpdated":null}');
+const __pageData = JSON.parse('{"title":"Rokae 双臂 ROS 2 接口技术文档","description":"","frontmatter":{},"headers":[],"relativePath":"ROS2_INTERFACE_REFERENCE.md","filePath":"ROS2_INTERFACE_REFERENCE.md","lastUpdated":1788509150000}');
 const _sfc_main = { name: "ROS2_INTERFACE_REFERENCE.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 179)
+    createStaticVNode("", 221)
   ])]);
 }
 const ROS2_INTERFACE_REFERENCE = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
