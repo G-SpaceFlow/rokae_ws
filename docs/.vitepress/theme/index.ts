db430e6962f1d90f392cf6325260401a27b6a2ec
Hamster-Li <1330983040@qq.com>
2026-09-07 15:56:54 +0800

    docs: scope right outline to selected sidebar function

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import SectionOutline from './SectionOutline.vue'
 import './style.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout: () => h(DefaultTheme.Layout, null, {
+    'aside-outline-before': () => h(SectionOutline),
+  }),
+}
